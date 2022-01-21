bbdb8337cf050c7d724c7c5434777a50e62cb77a
Wicki07 <71140843+Wicki07@users.noreply.github.com>
2022-01-21 17:21:41 +0100

    Update settings.py

--- backend/vuengo/settings.py
+++ backend/vuengo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
